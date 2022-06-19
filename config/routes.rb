ed58958dbca931540a33f23cc910e132759c0970
EC2 Default User <104773179+kazuwaki@users.noreply.github.com>
2022-06-19 11:52:05 +0000

    [finish]日付検索

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
       get :follows, :followers
     end
     resource :relationships, only: [:create, :destroy]
+    get "search", to: "users#search"
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
